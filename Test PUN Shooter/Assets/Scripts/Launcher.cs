a5bc1fb6948eafed8c6da29b1dbecce4b85c7f45
Gonzalo Montero <gonmon654@gmail.com>
2022-04-04 14:43:54 -0300

    Added Main Menu And Startup Connections

---
+++ Test PUN Shooter/Assets/Scripts/Launcher.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+    }
+}
