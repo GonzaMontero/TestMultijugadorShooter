ef54fca93fd4c6cacef409566851a0a97ec86dfc
Gonzalo Montero <gonmon654@gmail.com>
2022-04-04 15:30:08 -0300

    Finished Main Menu with Photon Connection

--- Test PUN Shooter/Assets/Scripts/Launcher.cs
+++ Test PUN Shooter/Assets/Scripts/Launcher.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorCode;
+    [SerializeField] TMP_Text roomNameText;
+    
     private void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
@@ -18,6 +23,40 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public override void OnJoinedLobby()
     {
+        MenuManager.Instance.OpenMenu("Title");
         Debug.Log("Joined Lobby");
+    }
+
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("Loading");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu("Room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        MenuManager.Instance.OpenMenu("Error");
+        errorCode.text = "Room Creation Failed" + message;
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("Loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("Title");
     }
 }
